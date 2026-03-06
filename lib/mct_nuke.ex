10b4e2edf3318b416a0a22fe19b38501a8a3c16c
Adrian Irving-Beer <wisq@wisq.net>
2026-03-05 21:45:49 -0500

    `mix new mct_nuke`

---
+++ lib/mct_nuke.ex
@@ -0,0 +1,18 @@
+defmodule MctNuke do
+  @moduledoc """
+  Documentation for `MctNuke`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MctNuke.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mct_nuke_test.exs
@@ -0,0 +1,8 @@
+defmodule MctNukeTest do
+  use ExUnit.Case
+  doctest MctNuke
+
+  test "greets the world" do
+    assert MctNuke.hello() == :world
+  end
+end
