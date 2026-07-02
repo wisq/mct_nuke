0af79c4da5b305211b9fa39337b8bcf8b8716966
Adrian Irving-Beer <wisq@wisq.net>
2026-07-02 13:27:02 -0400

    Add valve panel to stats collection.

--- lib/mct_nuke/collector.ex
+++ lib/mct_nuke/collector.ex
@@ -41,11 +41,9 @@ defmodule MctNuke.Collector do
       ts = API.get_integer("TIME_STAMP")
 
       if Stats.is_new?(stats, ts) do
-        %{"values" => values, "errors" => errors} = API.get_json("WEBSERVER_BATCH_GET")
-
-        unless Enum.empty?(errors) do
-          Logger.warning(@log_prefix <> "Errors detected: #{inspect(errors)}")
-        end
+        values =
+          get_batch_vars()
+          |> Map.put("VALVE_PANEL", get_valve_panel())
 
         stats =
           stats
@@ -72,6 +70,80 @@ defmodule MctNuke.Collector do
         {:noreply, stats}
     end
   end
+
+  defp get_batch_vars do
+    %{"values" => values, "errors" => errors} = API.get_json("WEBSERVER_BATCH_GET")
+
+    unless Enum.empty?(errors) do
+      Logger.warning(@log_prefix <> "Errors detected: #{inspect(errors)}")
+    end
+
+    values
+  end
+
+  defp get_valve_panel do
+    API.get_json("VALVE_PANEL_JSON")
+    |> Enum.map(fn {vp_type, vp_entries} ->
+      vp_type_atom = String.to_existing_atom(vp_type)
+
+      vp_entries
+      |> Enum.map(fn {key, data} ->
+        case valve_panel_metrics(vp_type_atom, data) do
+          nil -> nil
+          values when is_map(values) -> {key, values}
+        end
+      end)
+      |> Enum.reject(&is_nil/1)
+      |> then(fn
+        [] -> nil
+        [_ | _] = entries -> {vp_type_atom, Map.new(entries)}
+      end)
+    end)
+    |> Enum.reject(&is_nil/1)
+    |> Map.new()
+  end
+
+  @pump_states [
+    active: "Active",
+    destroyed: "Destroyed",
+    dry: "Dry",
+    energy_sufficent: "EnergySufficent",
+    flooded: "Flooded",
+    maintenance_required: "MaintenanceRequired",
+    overload: "Overload",
+    set_speed_reached: "SetSpeedReached",
+    under_construction: "UnderConstruction"
+  ]
+
+  defp valve_panel_metrics(:pumps, %{"Flow" => [flow_curr, flow_max], "State" => states}) do
+    %{
+      flow_rate: flow_curr,
+      capacity: flow_max,
+      speed: 100 * flow_curr / flow_max,
+      state:
+        @pump_states
+        |> Enum.map(fn {metric, data_key} ->
+          {metric, Map.fetch!(states, data_key)}
+        end)
+        |> Map.new()
+    }
+  end
+
+  defp valve_panel_metrics(:vessels, %{
+         "Pressure" => pressure,
+         "Temperature" => temperature,
+         "Volume" => [vol_curr, vol_max]
+       }) do
+    %{
+      pressure: pressure,
+      temperature: temperature,
+      volume: vol_curr,
+      fill_level: 100 * vol_curr / vol_max
+    }
+  end
+
+  defp valve_panel_metrics(:valves, _data), do: nil
+  defp valve_panel_metrics(:pipes, _data), do: nil
 
   defp schedule_next, do: Process.send_after(self(), :loop, @loop_ms)
 
