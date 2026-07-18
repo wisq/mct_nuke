00970510a42148f05b095ab7ed7659b926adce36
Adrian Irving-Beer <wisq@wisq.net>
2026-07-18 10:28:59 -0400

    Use localhost as default API target.

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,6 @@ import Config
 
 config :logger, :console, format: "$time $metadata[$level] $message\n"
 
-config :mct_nuke, api_url: "http://rotarran.wisq.org:8785"
+config :mct_nuke, api_url: "http://localhost:8785"
 
 import_config "#{Mix.env()}.exs"
